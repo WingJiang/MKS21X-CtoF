a946552ffd1dd000c33bf3026eba1b0bb397a631
wjiang30@stuy.edu <wjiang30@stuy.edu>
2021-09-23 21:33:32 -0400

    Code for celsiusToFahrenheit was written

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,5 +1,7 @@
 //The parameters of the function should be celsius and fahrenheit. The type should be a double. The functions return a value that is a double.
 
 public class CtoFTester {
-    
+    public double celsiusToFahrenheit(double celsius) {
+        return celsius * 9 / 5 + 32;
+    }
 }
